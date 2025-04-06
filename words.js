a6b0663c92abf30274e3b642bca09f82a3e9a90d
IbongAdarnaMobile <ibonarana5@gmail.com>
2025-04-06 09:23:13 +0800

    Add files via upload

---
+++ words.js
@@ -0,0 +1,58 @@
+const words = [
+    {
+        word: "buwan",
+        hint: "Isa sa 12 hati ng isang taon na binubuo ng apat na linggo na mayroong 28 hanggang 31 araw."
+    },
+    {
+        word: "lakbay",
+        hint: "Pagtungo sa malayong pook (lalo na upang makipagsapalaran o makakita ng magagandang tanawin"
+    },
+    {
+        word: "sanga",
+        hint: "Bahagi ng punongkahoy o anumang halaman na tumutubo sa pinakakatawan at inuusbungan naman ng mga dahon."
+    },
+    {
+        word: "balahibo",
+        hint: "Malambot na materyal na bumabalot sa mga ibon."
+    },
+    {
+        word: "iglap",
+        hint: "Isang maikling sandali; panahon na napakabilis naganap."
+    },
+    {
+        word: "kaharian",
+        hint: "Bansa o bayang pinamumunuan ng isang hari."
+    },
+    {
+        word: "narating",
+        hint: "Ang kakayahang makapunta sa isang lugar, punto, kalagayan, o makamit ang isang bagay."
+    },
+    {
+        word: "labis",
+        hint: "Kahigitan sa takdang sukat o dami."
+    },
+    {
+        word: "dumadapo",
+        hint: "bumaba at humimpil sa isang lugar o bagay mula sa paglipad o paggalaw, katulad ng pagpapahinga ng ibon sa sanga."
+    },
+    {
+        word: "pahinga",
+        hint: "Pagtigil sa paggawa ng isang napapagod upang makaipon ng lakas."
+    },
+    {
+        word: "nasaksihan",
+        hint: "Ang direktang pagkakita o pagdanas ng isang pangyayari, gawain, o kaganapan, na nagbibigay ng personal na kaalaman o karanasan sa pamamagitan ng pagiging naroroon."
+    },
+    {
+        word: "lamyos",
+        hint: "Masuyong haplos."
+    },
+    {
+        word: "napaidlip",
+        hint: "sandaling makatulog nang hindi sinasadya."
+    },
+    {
+        word: "Napatakan",
+        hint: "Matuluan, malagpakan."
+    },
+]
