7ccd688f6f7003304a14ba65cbbadd6a78b4baff
IbongAdarnaMobile <ibonarana5@gmail.com>
2025-04-08 18:16:15 +0800

    Update forscript.js

--- forscript.js
+++ forscript.js
@@ -26,7 +26,7 @@ const initTimer = maxTime => {
 };
 
 const initGame = () => {
-    initTimer(16);
+    initTimer(26);
     let randomObj = words[Math.floor(Math.random() * words.length)];
     let wordArray = randomObj.word.split("");
     for (let i = wordArray.length - 1; i > 0; i--) {
